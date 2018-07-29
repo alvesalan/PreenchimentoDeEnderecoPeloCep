940da96fe5aaa11ccaaf4f7759aaa5c67be46f3a
alvesalan <38699636+alvesalan@users.noreply.github.com>
2018-07-29 13:10:47 -0300

    Funcionado

---
+++ CEP/CEP/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CEP
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string urlConsulta = "http://cep.republicavirtual.com.br/web_cep.php?cep=@cep&formato=xml";
+
+            DataSet dsRetornaEndereco = new DataSet();
+            dsRetornaEndereco.ReadXml(urlConsulta.Replace("@cep", txtCep.Text));
+
+            string retorno = dsRetornaEndereco.Tables[0].Rows[0]["resultado"].ToString();
+
+            if (retorno == "0")
+            {
+                MessageBox.Show("CEP INVALIDO");
+            }
+            else
+            {
+                txtbairro.Text = dsRetornaEndereco.Tables[0].Rows[0]["Bairro"].ToString();
+                txtCidade.Text = dsRetornaEndereco.Tables[0].Rows[0]["cidade"].ToString();
+                txtLogadouro.Text = dsRetornaEndereco.Tables[0].Rows[0]["logradouro"].ToString();
+                txtTipoLogadouro.Text = dsRetornaEndereco.Tables[0].Rows[0]["tipo_logradouro"].ToString();
+                txtUF.Text = dsRetornaEndereco.Tables[0].Rows[0]["uf"].ToString();
+            }
+        }
+    }
+}
